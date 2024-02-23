0f8e8a7bb7d95c462064aba2d768d3e5210c51b3
bopzen <boppp@abv.bg>
2024-02-23 14:28:12 +0200

    - Recruiter Dashboard step by step building the functionality

--- CV_Link/profile_recruiter/views.py
+++ CV_Link/profile_recruiter/views.py
@@ -1,5 +1,13 @@
 from django.views import generic
+from django.contrib.auth import mixins as auth_mixins
+
+from CV_Link.profile_recruiter.models import RecruiterProfile
 
 
-class RecruiterDashboardPageView(generic.TemplateView):
+class RecruiterDashboardPageView(auth_mixins.LoginRequiredMixin, generic.DetailView):
+    model = RecruiterProfile
     template_name = 'recruiter-profile-dashboard.html'
+    context_object_name = 'recruiter_profile'
+
+    def get_object(self, queryset=None):
+        return RecruiterProfile.objects.get(user_id=self.request.user)
