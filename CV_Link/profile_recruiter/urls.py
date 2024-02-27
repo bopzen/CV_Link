9fafa816987770895f09bc11df25d06718cc9cb8
bopzen <boppp@abv.bg>
2024-02-27 11:23:55 +0200

    - Added Recruiter Profile edit view - Started adding recruiter address edit view

--- CV_Link/profile_recruiter/urls.py
+++ CV_Link/profile_recruiter/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from CV_Link.profile_recruiter.views import RecruiterDashboardView
+from CV_Link.profile_recruiter.views import RecruiterDashboardView, RecruiterEditView, RecruiterAddressEditView
 
 urlpatterns = [
-    path('dashboard/', RecruiterDashboardView.as_view(), name='recruiter-dashboard')
+    path('dashboard/', RecruiterDashboardView.as_view(), name='recruiter-dashboard'),
+    path('edit-profile/', RecruiterEditView.as_view(), name='recruiter-edit-profile'),
+    path('edit-address/<int:pk>/', RecruiterAddressEditView.as_view(), name='recruiter-edit-address')
 ]
--- CV_Link/profile_recruiter/views.py
+++ CV_Link/profile_recruiter/views.py
@@ -1,7 +1,8 @@
+from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import mixins as auth_mixins
 
-from CV_Link.profile_recruiter.models import RecruiterProfile
+from CV_Link.profile_recruiter.models import RecruiterProfile, Address
 
 
 class RecruiterDashboardView(auth_mixins.LoginRequiredMixin, generic.DetailView):
@@ -11,3 +12,29 @@ class RecruiterDashboardView(auth_mixins.LoginRequiredMixin, generic.DetailView)
 
     def get_object(self, queryset=None):
         return RecruiterProfile.objects.get(user_id=self.request.user)
+
+
+class RecruiterEditView(auth_mixins.LoginRequiredMixin, generic.UpdateView):
+    model = RecruiterProfile
+    template_name = 'recruiter-profile-edit.html'
+    fields = ['company_name', 'company_description', 'company_website']
+
+    def get_object(self, queryset=None):
+        return RecruiterProfile.objects.get(user_id=self.request.user)
+
+    def get_success_url(self):
+        return reverse_lazy('recruiter-dashboard')
+
+
+class RecruiterAddressEditView(auth_mixins.LoginRequiredMixin, generic.UpdateView):
+    model = Address
+    template_name = 'recruiter-address-edit.html'
+    fields = ['address_line1', 'address_line2', 'city', 'postal_code', 'country']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recruiter'] = self.object.recruiter
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('recruiter-dashboard')
