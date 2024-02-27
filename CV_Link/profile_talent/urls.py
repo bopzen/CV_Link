539dc96b6db5cd9aef7a4ce7a29a6ce3862e31cb
bopzen <boppp@abv.bg>
2024-02-27 17:09:08 +0200

    - Added Talent Profile edit view

--- CV_Link/profile_talent/urls.py
+++ CV_Link/profile_talent/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from CV_Link.profile_talent.views import TalentDashboardView
+from CV_Link.profile_talent.views import TalentDashboardView, TalentEditView
 
 urlpatterns = [
-    path('dashboard/', TalentDashboardView.as_view(), name='talent-dashboard')
+    path('dashboard/', TalentDashboardView.as_view(), name='talent-dashboard'),
+    path('edit-profile/', TalentEditView.as_view(), name='talent-edit-profile'),
+
 ]
--- CV_Link/profile_talent/views.py
+++ CV_Link/profile_talent/views.py
@@ -1,12 +1,26 @@
+from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import mixins as auth_mixins
 
 from CV_Link.profile_talent.models import TalentProfile
 
 
-class TalentDashboardView(generic.TemplateView):
+class TalentDashboardView(auth_mixins.LoginRequiredMixin, generic.DetailView):
     model = TalentProfile
     template_name = 'talent-profile-dashboard.html'
     context_object_name = 'talent_profile'
 
     def get_object(self, queryset=None):
         return TalentProfile.objects.get(user_id=self.request.user)
+
+
+class TalentEditView(auth_mixins.LoginRequiredMixin, generic.UpdateView):
+    model = TalentProfile
+    template_name = 'talent-profile-edit.html'
+    fields = ['first_name', 'last_name', 'birth_date', 'city', 'introduction']
+
+    def get_object(self, queryset=None):
+        return TalentProfile.objects.get(user_id=self.request.user)
+
+    def get_success_url(self):
+        return reverse_lazy('talent-dashboard')
