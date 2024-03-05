f808f12aa89b7e0f62dbdf48a6eb25ffac3fb93f
bopzen <boppp@abv.bg>
2024-03-05 13:05:13 +0200

    - Added Talent Education delete view

--- CV_Link/profile_talent/urls.py
+++ CV_Link/profile_talent/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from CV_Link.profile_talent.views import TalentDashboardView, TalentEditView, TalentEducationCreateView, \
-    TalentEducationEditView
+    TalentEducationEditView, TalentEducationDeleteView
 
 urlpatterns = [
     path('dashboard/', TalentDashboardView.as_view(), name='talent-dashboard'),
     path('edit-profile/', TalentEditView.as_view(), name='talent-edit-profile'),
     path('add-education/<int:pk>/', TalentEducationCreateView.as_view(), name='talent-create-education'),
     path('edit-education/<int:pk>/', TalentEducationEditView.as_view(), name='talent-edit-education'),
+    path('delete-education/<int:pk>/', TalentEducationDeleteView.as_view(), name='talent-delete-education'),
 ]
--- CV_Link/profile_talent/views.py
+++ CV_Link/profile_talent/views.py
@@ -60,3 +60,17 @@ class TalentEducationEditView(auth_mixins.LoginRequiredMixin, generic.UpdateView
     def form_valid(self, form):
         form.instance.talent = TalentProfile.objects.get(user=self.request.user)
         return super().form_valid(form)
+
+
+class TalentEducationDeleteView(auth_mixins.LoginRequiredMixin, generic.DeleteView):
+    model = Education
+    template_name = 'talent-education-delete.html'
+    fields = ['institution', 'degree', 'field_of_study', 'city', 'country', 'description', 'start_date', 'end_date']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['talent'] = TalentProfile.objects.get(user=self.request.user)
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('talent-dashboard')
